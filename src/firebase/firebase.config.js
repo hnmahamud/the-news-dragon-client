abeacdb841ec3e0cea976051bead893ae00fe5bd
hnmahamud <hnmahamud@gmail.com>
2023-04-29 03:24:32 +0600

    added env for firebaseConfig

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-R28oT3yZhCb8gWvdoYtSQWn-1MXwo-k",
-  authDomain: "the-news-dragon-cc413.firebaseapp.com",
-  projectId: "the-news-dragon-cc413",
-  storageBucket: "the-news-dragon-cc413.appspot.com",
-  messagingSenderId: "405734801996",
-  appId: "1:405734801996:web:9a04ed64ef8730b2866ae9",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
